1e9413a72236c23d9142038111817eb73a1dc1b2
TB-effective <55880682+TB-effective@users.noreply.github.com>
2024-04-12 20:48:12 +0200

    Update comment.js to switch to Markdown-IT library
    https://bugzilla.vircon.net/show_bug.cgi?id=13910
    Almost drop-in replacement for old and unmaintained markdown-js lib

--- Markdownjs/web/comment.js
+++ Markdownjs/web/comment.js
@@ -3,12 +3,21 @@
 'use strict';
 /*jslint node:true, indent:2, nomen:true*/
 
+// https://bugzilla.vircon.net/show_bug.cgi?id=13910                                                                                                                                   
+// Switch to Markdown-it and set some options the way we want them.                                                                                                                    
+const md = markdownit({
+    html: true,
+    linkify: true,
+    breaks: true
+  }
+);
+
 $('.bz_comment_text').each(function () {
   var elm = $(this);
   elm
     .after(
       '<div style="markdown markdown-comment" id="' + elm.attr('id') + '_markdown">' +
-        markdown.toHTML(elm.text()) +
+        md.render(elm.text()) +
       '</div><a href="#' + elm.attr('id') +
       '" class="markdown-btn">Show Raw</a>'
     )
@@ -28,12 +37,12 @@ $('.markdown-btn').click(function (e) {
 });
 
 $('#comment').on('input', function () {
-  $('#bz_preview').html(markdown.toHTML($(this).val()));
+  $('#bz_preview').html(md.render($(this).val()));
 });
 
 $('.markdown-help').each(function (elm) {
   var elm = $(this);
-  elm.html(markdown.toHTML(elm.text()));
+  elm.html(md.render(elm.text()));
 });
 
 $('#syntax-help-btn').click(function (e) {
